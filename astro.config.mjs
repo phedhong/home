c06a2cbfc9dfde624d7811d54b9b3689a7e44f6b
ed <42587998+phedhong@users.noreply.github.com>
2023-11-18 03:24:33 -0600

    Git Action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://github.com/phedhong.github.io',
+  base: '/MyAstro',
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+});
+
