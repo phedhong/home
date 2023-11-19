b5f0acb5352c116d9ec1a9d620c957c9685079ff
ed <42587998+phedhong@users.noreply.github.com>
2023-11-18 19:13:20 -0600

    Test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://phedhong.github.io',
   base: '/MyAstro',
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
 });
 
